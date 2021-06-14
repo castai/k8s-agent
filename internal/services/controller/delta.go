248f5242b85edc60d695a35132d4b4c51a5e6ada
Saulius Mašnauskas <smasnauskas@gmail.com>
2021-06-14 12:55:52 +0300

    feat: debounce delta items

---
+++ internal/services/controller/delta.go
@@ -0,0 +1,140 @@
+package controller
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"castai-agent/internal/castai"
+)
+
+func newDelta(log logrus.FieldLogger, clusterID, clusterVersion string) *delta {
+	return &delta{
+		log:            log,
+		clusterID:      clusterID,
+		clusterVersion: clusterVersion,
+		fullSnapshot:   true,
+		cache:          map[string]*item{},
+	}
+}
+
+type delta struct {
+	log            logrus.FieldLogger
+	clusterID      string
+	clusterVersion string
+	fullSnapshot   bool
+	cache          map[string]*item
+}
+
+func (d *delta) add(i *item) {
+	key := mustKeyObject(i.obj)
+
+	if other, ok := d.cache[key]; ok && other.event == eventAdd && i.event == eventDelete {
+		delete(d.cache, key)
+	} else if ok && other.event == eventAdd && i.event == eventUpdate {
+		i.event = eventAdd
+		d.cache[key] = i
+	} else if ok && other.event == eventDelete && (i.event == eventAdd || i.event == eventUpdate) {
+		i.event = eventUpdate
+		d.cache[key] = i
+	} else {
+		d.cache[key] = i
+	}
+}
+
+func (d *delta) clear() {
+	d.fullSnapshot = false
+	d.cache = map[string]*item{}
+}
+
+func (d *delta) toCASTAIRequest() *castai.Delta {
+	var items []*castai.DeltaItem
+
+	for _, i := range d.cache {
+		data, err := encode(i.obj)
+		if err != nil {
+			d.log.Errorf("failed to encode %T: %v", i.obj, err)
+			continue
+		}
+
+		kinds, _, err := scheme.ObjectKinds(i.obj)
+		if err != nil {
+			d.log.Errorf("failed to find object %T kind: %v", i.obj, err)
+			continue
+		}
+		if len(kinds) == 0 || kinds[0].Kind == "" {
+			d.log.Errorf("unknown object kind for object %T", i.obj)
+			continue
+		}
+
+		items = append(items, &castai.DeltaItem{
+			Event:     i.event.toCASTAIEvent(),
+			Kind:      kinds[0].Kind,
+			Data:      data,
+			CreatedAt: time.Now().UTC(),
+		})
+	}
+
+	return &castai.Delta{
+		ClusterID:      d.clusterID,
+		ClusterVersion: d.clusterVersion,
+		FullSnapshot:   d.fullSnapshot,
+		Items:          items,
+	}
+}
+
+func encode(obj interface{}) (string, error) {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		return "", fmt.Errorf("marshaling %T to json: %v", obj, err)
+	}
+	return base64.StdEncoding.EncodeToString(b), nil
+}
+
+type item struct {
+	obj   runtime.Object
+	event event
+}
+
+type event string
+
+const (
+	eventAdd    event = "add"
+	eventDelete event = "delete"
+	eventUpdate event = "update"
+)
+
+func (e event) toCASTAIEvent() castai.EventType {
+	switch e {
+	case eventAdd:
+		return castai.EventAdd
+	case eventDelete:
+		return castai.EventDelete
+	case eventUpdate:
+		return castai.EventUpdate
+	}
+	return ""
+}
+
+// keyObject generates a unique key for an object, for example: `*v1.Pod::namespace/name`.
+func keyObject(obj runtime.Object) (string, error) {
+	metaObj, ok := obj.(metav1.Object)
+	if !ok {
+		return "", fmt.Errorf("expected object of type %T to implement metav1.Object", obj)
+	}
+	return fmt.Sprintf("%s::%s/%s", reflect.TypeOf(obj).String(), metaObj.GetNamespace(), metaObj.GetName()), nil
+}
+
+func mustKeyObject(obj runtime.Object) string {
+	k, err := keyObject(obj)
+	if err != nil {
+		panic(fmt.Errorf("getting object key: %w", err))
+	}
+	return k
+}
---
+++ internal/services/controller/delta_test.go
@@ -0,0 +1,222 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"castai-agent/internal/castai"
+)
+
+func TestDelta(t *testing.T) {
+	clusterID := uuid.New().String()
+	version := "1.18"
+
+	pod1 := &v1.Pod{ObjectMeta: metav1.ObjectMeta{Namespace: v1.NamespaceDefault, Name: "a"}}
+	pod1Updated := &v1.Pod{ObjectMeta: metav1.ObjectMeta{Namespace: v1.NamespaceDefault, Name: "a", Labels: map[string]string{"a": "b"}}}
+
+	pod2 := &v1.Pod{ObjectMeta: metav1.ObjectMeta{Namespace: v1.NamespaceDefault, Name: "b"}}
+
+	tests := []struct {
+		name     string
+		items    []*item
+		expected *castai.Delta
+	}{
+		{
+			name:  "empty items",
+			items: []*item{},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+			},
+		},
+		{
+			name: "multiple items",
+			items: []*item{
+				{
+					obj:   pod1,
+					event: eventAdd,
+				},
+				{
+					obj:   pod2,
+					event: eventAdd,
+				},
+			},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+				Items: []*castai.DeltaItem{
+					{
+						Event: castai.EventAdd,
+						Kind:  "Pod",
+						Data:  mustEncode(t, pod1),
+					},
+					{
+						Event: castai.EventAdd,
+						Kind:  "Pod",
+						Data:  mustEncode(t, pod2),
+					},
+				},
+			},
+		},
+		{
+			name: "debounce: override added item with updated data",
+			items: []*item{
+				{
+					obj:   pod1,
+					event: eventAdd,
+				},
+				{
+					obj:   pod1Updated,
+					event: eventUpdate,
+				},
+			},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+				Items: []*castai.DeltaItem{
+					{
+						Event: castai.EventAdd,
+						Kind:  "Pod",
+						Data:  mustEncode(t, pod1Updated),
+					},
+				},
+			},
+		},
+		{
+			name: "debounce: entirely remove added item when it is deleted",
+			items: []*item{
+				{
+					obj:   pod1,
+					event: eventAdd,
+				},
+				{
+					obj:   pod1,
+					event: eventDelete,
+				},
+			},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+			},
+		},
+		{
+			name: "debounce: keep only delete event when an updated item is deleted",
+			items: []*item{
+				{
+					obj:   pod1,
+					event: eventUpdate,
+				},
+				{
+					obj:   pod1,
+					event: eventDelete,
+				},
+			},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+				Items: []*castai.DeltaItem{
+					{
+						Event: castai.EventDelete,
+						Kind:  "Pod",
+						Data:  mustEncode(t, pod1),
+					},
+				},
+			},
+		},
+		{
+			name: "debounce: override updated item with newer updated data",
+			items: []*item{
+				{
+					obj:   pod1,
+					event: eventUpdate,
+				},
+				{
+					obj:   pod1Updated,
+					event: eventUpdate,
+				},
+			},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+				Items: []*castai.DeltaItem{
+					{
+						Event: castai.EventUpdate,
+						Kind:  "Pod",
+						Data:  mustEncode(t, pod1Updated),
+					},
+				},
+			},
+		},
+		{
+			name: "debounce: change deleted item to updated when it is readded",
+			items: []*item{
+				{
+					obj:   pod1,
+					event: eventDelete,
+				},
+				{
+					obj:   pod1Updated,
+					event: eventAdd,
+				},
+			},
+			expected: &castai.Delta{
+				ClusterID:      clusterID,
+				ClusterVersion: version,
+				FullSnapshot:   true,
+				Items: []*castai.DeltaItem{
+					{
+						Event: castai.EventUpdate,
+						Kind:  "Pod",
+						Data:  mustEncode(t, pod1Updated),
+					},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			d := newDelta(logrus.New(), clusterID, version)
+
+			for _, item := range test.items {
+				d.add(item)
+			}
+
+			got := d.toCASTAIRequest()
+
+			require.Equal(t, clusterID, got.ClusterID)
+			require.Equal(t, version, got.ClusterVersion)
+			require.True(t, got.FullSnapshot)
+			require.Equal(t, len(got.Items), len(test.expected.Items))
+			for _, expectedItem := range test.expected.Items {
+				requireContains(t, got.Items, expectedItem)
+			}
+		})
+	}
+}
+
+func mustEncode(t *testing.T, obj interface{}) string {
+	data, err := encode(obj)
+	require.NoError(t, err)
+	return data
+}
+
+func requireContains(t *testing.T, actual []*castai.DeltaItem, expected *castai.DeltaItem) {
+	for _, di := range actual {
+		if di.Kind == expected.Kind && di.Event == expected.Event && di.Data == expected.Data {
+			return
+		}
+	}
+	require.Failf(t, "failed", "expected %s to contain %s", actual, expected)
+}
