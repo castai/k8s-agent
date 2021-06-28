587c5b08846016359b468403dfee87dc2c43aaa2
Zilvinas Urbonas <zilvinas.urbon@gmail.com>
2021-06-28 22:02:40 +0300

    chore: formatting

--- pkg/log/exporter_test.go
+++ pkg/log/exporter_test.go
@@ -32,7 +32,23 @@ func TestSetupLogExporter(t *testing.T) {
 		}).Times(1)
 		log := logger.WithFields(logrus.Fields{
 			"cluster_id": mockClusterID,
-			"provider": "eks",
+			"provider":   "eks",
+		})
+		log.Log(logrus.ErrorLevel, "failed to discover account id")
+		time.Sleep(1 * time.Second)
+	})
+
+	t.Run("fails to send the log msg", func(t *testing.T) {
+		mockapi.EXPECT().SendLogEvent(gomock.Any(), gomock.Any(), gomock.Any()).
+			DoAndReturn(func(_ context.Context, clusterID string, req *castai.IngestAgentLogsRequest) *castai.IngestAgentLogsResponse {
+				fields := req.LogEvent.Fields
+				require.Equal(t, mockClusterID, fields["cluster_id"])
+				require.Equal(t, "eks", fields["provider"])
+				return &castai.IngestAgentLogsResponse{}
+			}).Times(1)
+		log := logger.WithFields(logrus.Fields{
+			"cluster_id": mockClusterID,
+			"provider":   "eks",
 		})
 		log.Log(logrus.ErrorLevel, "failed to discover account id")
 		time.Sleep(1 * time.Second)
