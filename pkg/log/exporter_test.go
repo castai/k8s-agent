e9779ac56d862c5b7442b2d470015928e4f5e5a2
Žilvinas Urbonas <zilvinas@cast.ai>
2021-06-30 10:48:16 +0300

    feat(logging): export logs (#30)
    Co-authored-by: jmazionis <julius.mazionis@gmail.com>

---
+++ pkg/log/exporter_test.go
@@ -0,0 +1,59 @@
+package log
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/require"
+
+	"castai-agent/internal/castai"
+	mock_castai "castai-agent/internal/castai/mock"
+)
+
+func TestSetupLogExporter(t *testing.T) {
+	logger, hook := test.NewNullLogger()
+	defer hook.Reset()
+	mockClusterID := uuid.New().String()
+	ctrl := gomock.NewController(t)
+	mockapi := mock_castai.NewMockClient(ctrl)
+	SetupLogExporter(logger, mockapi, Config{ClusterID: mockClusterID, MsgSendTimeoutSecs: 1})
+
+	t.Run("sends the log msg", func(t *testing.T) {
+		r := require.New(t)
+		mockapi.EXPECT().SendLogEvent(gomock.Any(), gomock.Any(), gomock.Any()).
+			Do(assertClusterFields(r, mockClusterID, "eks")).Return(&castai.IngestAgentLogsResponse{}).Times(1)
+		log := logger.WithFields(logrus.Fields{
+			"cluster_id": mockClusterID,
+			"provider":   "eks",
+		})
+		log.Log(logrus.ErrorLevel, "failed to discover account id")
+		time.Sleep(1 * time.Second)
+	})
+
+	t.Run("skips sending log msg", func(t *testing.T) {
+		mockapi.EXPECT().SendLogEvent(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+		log := logrus.WithContext(castai.DoNotSendLogsCtx())
+		log = log.WithFields(logrus.Fields{
+			"cluster_id": mockClusterID,
+			"provider":   "eks",
+		})
+		log.Log(logrus.ErrorLevel, "failed to discover account id")
+		time.Sleep(1 * time.Second)
+	})
+}
+
+func assertClusterFields(
+	r *require.Assertions, mockClusterID, provider string,
+) func(_ context.Context, clusterID string, req *castai.IngestAgentLogsRequest) *castai.IngestAgentLogsResponse {
+	return func(_ context.Context, clusterID string, req *castai.IngestAgentLogsRequest) *castai.IngestAgentLogsResponse {
+		fields := req.LogEvent.Fields
+		r.Equal(mockClusterID, fields["cluster_id"])
+		r.Equal(provider, fields["provider"])
+		return &castai.IngestAgentLogsResponse{}
+	}
+}
