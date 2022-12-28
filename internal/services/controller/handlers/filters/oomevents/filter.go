04df31f325ad0ddb97c13c1e85f96aed9e017bec
Valdas Rakutis <valdas@rakutis.lt>
2022-12-28 14:07:44 +0200

    remove unneccessary util pkg

--- internal/services/controller/handlers/filters/oomevents/filter.go
+++ internal/services/controller/handlers/filters/oomevents/filter.go
@@ -1,17 +1,46 @@
 package oomevents
 
 import (
+	"strings"
+
 	corev1 "k8s.io/api/core/v1"
 
 	"castai-agent/internal/castai"
-	"castai-agent/internal/services/controller/util"
 )
 
-func Filter(e castai.EventType, obj interface{}) bool {
-	k8sEvent, ok := obj.(*corev1.Event)
+const (
+	KindPod           = "Pod"
+	ReasonOOMEviction = "Evicted"
+
+	AnnotationStarvedResource = "starved_resource"
+
+	ResourceMemory = "memory"
+)
+
+func Filter(_ castai.EventType, obj interface{}) bool {
+	event, ok := obj.(*corev1.Event)
 	if !ok {
 		return false
 	}
 
-	return util.IsOOMEvent(k8sEvent)
+	if event.Reason != ReasonOOMEviction {
+		return false
+	}
+
+	if event.InvolvedObject.Kind != KindPod {
+		return false
+	}
+
+	if event.Annotations == nil {
+		return false
+	}
+
+	// starvedResourcesString contains a list of starved resources separated by commas.
+	starvedResourcesString, starvedResourcesFound := event.Annotations[AnnotationStarvedResource]
+	if !starvedResourcesFound {
+		return false
+	}
+
+	return strings.Contains(starvedResourcesString, ResourceMemory)
+
 }
