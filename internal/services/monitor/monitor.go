20331a939c1d2c971de49bb6d307805be836fb6c
Viktoras <viktoras@cast.ai>
2023-05-25 10:34:48 +0300

    wip

--- internal/services/monitor/monitor.go
+++ internal/services/monitor/monitor.go
@@ -60,7 +60,8 @@ func (m *monitor) metadataUpdated(ctx context.Context, metadata Metadata) {
 func (m *monitor) reportPodDiagnostics(ctx context.Context, prevProcessPID uint64) {
 	m.log.Errorf("unexpected agent restart detected, fetching k8s events for %s/%s", m.pod.Namespace, m.pod.Name)
 
-	m.logEvents(ctx, m.log.WithField("events_group", fmt.Sprintf("%s/%s", m.pod.Namespace, m.pod.Name)), &metav1.ListOptions{
+	// log pod-related warnings
+	m.logEvents(ctx, m.log.WithField("events_group", fmt.Sprintf("%s/%s", m.pod.Namespace, m.pod.Name)), m.pod.Namespace, &metav1.ListOptions{
 		FieldSelector: "involvedObject.name=" + m.pod.Name,
 		TypeMeta: metav1.TypeMeta{
 			Kind: "Pod",
@@ -68,28 +69,33 @@ func (m *monitor) reportPodDiagnostics(ctx context.Context, prevProcessPID uint6
 	}, func(event *v1.Event) bool {
 		return true
 	})
+
+	// Log node-related warnings. We can't find relevant messages easily as there's no metadata linking events to specific pods,
+	// and even filtering by PID id does not work (agent process PID is different inside the pod and as seen from the node).
+	// Instead, will use simple filtering by "castai-agent"; combined with node-name filter, this should be sufficient enough
+	// to narrow the list down to agent-related events only.
 	m.logEvents(ctx, m.log.WithFields(logrus.Fields{
 		"events_group": fmt.Sprintf("node/%s", m.pod.Node),
 		"pid":          prevProcessPID,
-	}), &metav1.ListOptions{
+	}), v1.NamespaceAll, &metav1.ListOptions{
 		FieldSelector: "involvedObject.name=" + m.pod.Node,
 		TypeMeta: metav1.TypeMeta{
 			Kind: "Node",
 		},
 	}, func(event *v1.Event) bool {
 		// OOM events are reported on the node, but the only relation to the pod is the killed process PID.
-		return strings.Contains(event.Message, fmt.Sprintf("%d", prevProcessPID))
+		return strings.Contains(event.Message, "castai-agent")
 	})
 }
 
-func (m *monitor) logEvents(ctx context.Context, log logrus.FieldLogger, listOptions *metav1.ListOptions, filter func(event *v1.Event) bool) {
-	events, err := m.clientset.CoreV1().Events(m.pod.Namespace).List(ctx, *listOptions)
+func (m *monitor) logEvents(ctx context.Context, log logrus.FieldLogger, namespace string, listOptions *metav1.ListOptions, filter func(event *v1.Event) bool) {
+	events, err := m.clientset.CoreV1().Events(namespace).List(ctx, *listOptions)
 	if err != nil {
 		log.Errorf("failed fetching k8s events after agent restart: %v", err)
 		return
 	}
 	relevantEvents := lo.Filter(events.Items, func(e v1.Event, _ int) bool {
-		return e.Type == v1.EventTypeNormal || !filter(&e)
+		return e.Type != v1.EventTypeNormal && filter(&e)
 	})
 
 	if len(relevantEvents) == 0 {
