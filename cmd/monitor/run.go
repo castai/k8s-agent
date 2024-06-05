0bdc9aae5a8bef36da05e0d3c08fc8b2e134d5b8
Mike Norgate <michael@cast.ai>
2024-06-05 14:09:55 +0100

    fix: required API details for snapshot-dump

--- cmd/monitor/run.go
+++ cmd/monitor/run.go
@@ -2,6 +2,7 @@ package monitor
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/sirupsen/logrus"
@@ -15,6 +16,12 @@ import (
 
 func run(ctx context.Context) error {
 	cfg := config.Get()
+	if cfg.API.Key == "" {
+		return errors.New("env variable \"API_KEY\" is required")
+	}
+	if cfg.API.URL == "" {
+		return errors.New("env variable \"API_URL\" is required")
+	}
 
 	remoteLogger := logrus.New()
 	remoteLogger.SetLevel(logrus.Level(cfg.Log.Level))
