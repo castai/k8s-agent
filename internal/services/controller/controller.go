36e6684505b0a53e4d9bccb83f99838df85d0736
Pijus Navickas <pijus@cast.ai>
2024-11-11 14:44:36 +0200

    feat: TypeMeta as primary object Kind value source
    This reverts commit 98d248f6c37d1d79cfaf44821c018e856b849aec.

--- internal/services/controller/controller.go
+++ internal/services/controller/controller.go
@@ -180,7 +180,7 @@ func CollectSingleSnapshot(ctx context.Context,
 		return nil, err
 	}
 
-	log.Debugf("synced %d items", len(d.CacheLegacy))
+	log.Debugf("synced %d items", len(d.Cache))
 
 	return d.ToCASTAIRequest(), nil
 }
@@ -514,7 +514,7 @@ func (c *Controller) send(ctx context.Context) {
 	nodesByName := map[string]*corev1.Node{}
 	var nodes []*corev1.Node
 
-	for _, item := range c.delta.CacheLegacy {
+	for _, item := range c.delta.Cache {
 		n, ok := item.Obj.(*corev1.Node)
 		if !ok {
 			continue
