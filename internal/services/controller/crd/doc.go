cf9e3ee6f1c1d3e3b6898a891f5c06a9cb8dccfa
Max Twardowski <max@cast.ai>
2025-06-30 13:56:30 +0200

    get rid of generate file, move go:generate to doc

--- internal/services/controller/crd/doc.go
+++ internal/services/controller/crd/doc.go
@@ -1,2 +1,4 @@
 // +k8s:deepcopy-gen=package
+//
+//go:generate deepcopy-gen . --output-file zz_generated.deepcopy.go
 package crd
--- internal/services/controller/crd/generate.go
+++
@@ -1,2 +0,0 @@
-//go:generate deepcopy-gen . --output-file zz_generated.deepcopy.go
-package crd
