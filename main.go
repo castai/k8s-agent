5654d5d87f0fcbbd9f35e54489332b0377cb4969
Karolis Labrencis <karolis@labrencis.lt>
2026-01-27 16:16:55 +0200

    feat: Support overriding GOMEMLIMIT ratio

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	_ "net/http/pprof"
+	"strconv"
 
 	"github.com/KimMachineGun/automemlimit/memlimit"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -9,11 +11,24 @@ import (
 
 	"castai-agent/cmd"
 	"castai-agent/internal/config"
+	"os"
 )
 
+const envGoMemLimitOverride = "GOMEMLIMIT_RATIO"
+
 func init() {
+	memlimitRatio := 0.8
+	if ratioOverride := os.Getenv(envGoMemLimitOverride); ratioOverride != "" {
+		val, err := strconv.ParseFloat(ratioOverride, 64)
+		if err == nil {
+			memlimitRatio = val
+		} else {
+			fmt.Fprintf(os.Stderr, "warning: failed to parse env var %q value %q as float: %v", envGoMemLimitOverride, ratioOverride, err)
+		}
+	}
+
 	_, _ = memlimit.SetGoMemLimitWithOpts(
-		memlimit.WithRatio(0.8),
+		memlimit.WithRatio(memlimitRatio),
 		memlimit.WithProvider(memlimit.ApplyFallback(
 			memlimit.FromCgroup,
 			memlimit.FromSystem,
